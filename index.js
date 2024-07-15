a2b0f58c8dc4e83661111a08d7791c1211e0d287
dkzhen <dani314123@gmail.com>
2024-07-16 05:36:06 +0700

    mod: update gitignore

---
+++ index.js
@@ -0,0 +1,21 @@
+const cron = require("node-cron");
+const express = require("express");
+const { claimMission } = require("./func/ClaimMission");
+const { claimOfflineProfit } = require("./func/ClaimOfflineProfit");
+
+// Schedule the task to run every hour on the hour
+claimMission();
+claimOfflineProfit()
+cron.schedule("0 * * * *", claimMission);
+cron.schedule("0 * * * *", claimOfflineProfit);
+// Start the server
+const port = process.env.PORT || 105;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API cron job server is running");
+});
+
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+});
