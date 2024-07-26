81fa4eaf4f5ce3c744e22b710953bceee1a2328a
dkzhen <dani314123@gmail.com>
2024-07-26 19:00:19 +0700

    mod: env

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const cron = require("node-cron");
 const express = require("express");
-const { getAuthToken } = require("./func/getAuthToken");
-const { validateToken } = require("./func/CheckValidToken");
+
 const { playGame } = require("./func/PlayGame");
 const { claimRewards } = require("./func/ClaimReward");
 const { DailyRewards } = require("./func/DailyReward");
 const { mission, claimMission } = require("./func/Mission");
-
+const { configDotenv } = require("dotenv");
+configDotenv();
 // Schedule the task to run every hour on the hour
 playGame();
 claimMission();
@@ -20,7 +20,7 @@ cron.schedule("0 * * * *", mission);
 cron.schedule("0 0 * * *", DailyRewards);
 
 // Start the server
-const port = process.env.PORT || 105;
+const port = process.env.PORT || 104;
 const app = express();
 
 app.get("/", (req, res) => {
